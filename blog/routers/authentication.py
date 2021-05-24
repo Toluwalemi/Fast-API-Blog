042c3aa8bae406395c35aa6d2a9c2df832509ea5
Toluwalemi <toluwalemisrael@gmail.com>
2021-05-24 02:00:32 +0100

    feat: Add authorization to blog endpoints

--- blog/routers/authentication.py
+++ blog/routers/authentication.py
@@ -1,10 +1,10 @@
 from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 
 from blog import models
 from blog.helpers.db_helpers import get_db
 from blog.helpers.hashing import verify
-from blog.schemas import Login
 from blog.token import create_access_token
 
 router = APIRouter(
@@ -13,7 +13,7 @@ router = APIRouter(
 
 
 @router.post('/login')
-def login(request: Login, db: Session = Depends(get_db)):
+def login(request: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
     user = db.query(models.User).filter(models.User.email == request.username).first()
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
--- blog/routers/blog.py
+++ blog/routers/blog.py
@@ -3,36 +3,39 @@ from typing import List
 from fastapi import APIRouter, Depends, status
 from sqlalchemy.orm import Session
 
-from blog import schemas
+from blog import schemas, oauth2
 from blog.helpers.blog_helper import get_all, create, destroy_helper, update_helper, show
 from blog.helpers.db_helpers import get_db
+from blog.schemas import User
 
 router = APIRouter(
-    prefix="/items",
+    prefix="/blog",
     tags=["blogs"]
 )
 
 
 @router.get('/', response_model=List[schemas.ShowBlog])
-def get_all_posts(db: Session = Depends(get_db)):
+def get_all_posts(db: Session = Depends(get_db), current_user: User = Depends(oauth2.get_current_user)):
     return get_all(db)
 
 
-@router.post('/', status_code=status.HTTP_201_CREATED, tags=["blogs"])
-def create_blog_post(request: schemas.Blog, db: Session = Depends(get_db)):
+@router.post('/', status_code=status.HTTP_201_CREATED)
+def create_blog_post(request: schemas.Blog, db: Session = Depends(get_db),
+                     current_user: User = Depends(oauth2.get_current_user)):
     return create(request, db)
 
 
-@router.delete('/{id}', status_code=status.HTTP_204_NO_CONTENT, tags=["blogs"])
-def destroy(id, db: Session = Depends(get_db)):
+@router.delete('/{id}', status_code=status.HTTP_204_NO_CONTENT)
+def destroy(id, db: Session = Depends(get_db), current_user: User = Depends(oauth2.get_current_user)):
     return destroy_helper(id, db)
 
 
-@router.put('/{id}', status_code=status.HTTP_202_ACCEPTED, tags=["blogs"])
-def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
+@router.put('/{id}', status_code=status.HTTP_202_ACCEPTED)
+def update(id, request: schemas.Blog, db: Session = Depends(get_db),
+           current_user: User = Depends(oauth2.get_current_user)):
     return update_helper(id, request, db)
 
 
-@router.get('/{id}', status_code=200, response_model=schemas.ShowBlog, tags=["blogs"])
-def get_single_id(id, db: Session = Depends(get_db)):
+@router.get('/{id}', status_code=200, response_model=schemas.ShowBlog)
+def get_single_id(id, db: Session = Depends(get_db), current_user: User = Depends(oauth2.get_current_user)):
     return show(id, db)
